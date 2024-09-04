5591e3864f75ded16e42e62e11607a333ee64d2a
MomoCode2 <mohamed-amine.mahboubi@polymtl.ca>
2024-09-04 16:24:00 -0400

    Réponse à la question 1 complétée

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+Aire = rayon * rayon * math.pi
+print(Aire)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Je m'appelle Mohamed A.M.")
